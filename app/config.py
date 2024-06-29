3aedafc6f2b0d53015fa1cf15847e4e908f13e9a
William Whitehead <willclashtest@gmail.com>
2024-06-29 16:26:50 -0500

    hi!

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# app/config.py
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+#PythonAnywhere MySQL Database URL
+
+
+#DATABASE_URL = "mysql+aiomysql://w1lt:H@wk2025!@w1lt.mysql.pythonanywhere-services.com/w1lt$hackku"
